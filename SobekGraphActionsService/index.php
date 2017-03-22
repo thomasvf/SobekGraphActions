9bbaf8ee6ce72cfd25085ff6b618b28271c9b225
thomasvf <thomasvf2@hotmail.com>
2017-03-22 19:26:50 -0300

    Adicionada data ao nome do arquivo

--- SobekGraphActionsService/index.php
+++ SobekGraphActionsService/index.php
@@ -20,11 +20,21 @@ function jsonFromPost(){
     }
 }
 
+function makeJsonFileName(){
+    $currentDate = date("d").date("m").date("y")."_".date("H").date("i");
+    $uniqueId = substr(uniqid(rand(), true), 20);
+
+    return $currentDate."_".$uniqueId;
+}
+
 function saveJson($jsonString){
-    $jsonFileHandler = fopen(GRAPH_ACTIONS_DIRECTORY.uniqid(rand()).".json", "w");
+    $fileName = makeJsonFileName();
+    $jsonFileHandler = fopen(GRAPH_ACTIONS_DIRECTORY.$fileName.".json", "w");
     fwrite($jsonFileHandler, $jsonString);
     fclose($jsonFileHandler);
 }
 
+date_default_timezone_set('America/Sao_Paulo');
 $jsonString = jsonFromPost();
 saveJson($jsonString);
+
